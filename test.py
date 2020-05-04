446cb42b4e5a0874167b4d0659f1caaf7e39fbe4
Pablo Soifer <pablo.soifer@wwecorp.com>
2020-05-04 20:06:45 -0300

    Clase 4

--- test.py
+++ test.py
@@ -5,23 +5,25 @@ import time
 tc = unittest.TestCase('__init__')
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('http://automationpractice.com/index.php')
-#search_box = driver.find_element_by_id('search_query_top')
-#search_box.send_keys('Hola Mundo')
 driver.find_element_by_id('search_query_top').send_keys('Hola Mundo')
-#search_box_button = driver.find_element_by_name('submit_search')
-#search_box_button.click()
 driver.find_element_by_name('submit_search').click()
 time.sleep(2)
-#results_label = driver.find_element_by_xpath('//*[@id="center_column"]/p')
-#tc.assertEqual('No results were found for your search "Hola Mundo"',results_label.text)
 tc.assertEqual('No results were found for your search "Hola Mundo"',driver.find_element_by_xpath('//*[@id="center_column"]/p').text)
-#women_link = driver.find_element_by_link_text('Women')
-#women_link.click()
 driver.find_element_by_link_text('Women').click()
 time.sleep(2)
-#dress_link = driver.find_element_by_partial_link_text('Dres')
-#dress_linl.click()
 driver.find_element_by_partial_link_text('res').click()
 time.sleep(2)
+#Link Text
+driver.find_element_by_link_text('Casual Dresses').click()
+#partial Link Text
+driver.find_element_by_partial_link_text('Casual').click()
+#por classname
+driver.find_elements_by_class_name('subcategory-name').click()
+#por cssselector
+driver.find_elements_by_css_selector('a.subcategory-name').click()
+#por xpath
+driver.find_element_by_xpath('//*[@id="subcategories"]/ul/li[1]/h5/a').click()
+#por tag
+driver.find_elements_by_tag_name('a').click()
 driver.close()
 driver.quit()
